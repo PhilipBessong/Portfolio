bb6f4a8c96048030910f07395fb5a6866029abdd
PhilipBessong <76492088+PhilipBessong@users.noreply.github.com>
2025-09-24 19:49:59 +0200

    About Page Added

--- SebsElite/site/sebselite/src/app/app.routes.ts
+++ SebsElite/site/sebselite/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
+import { About } from './about/about';
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'about', component: About },
 ];
