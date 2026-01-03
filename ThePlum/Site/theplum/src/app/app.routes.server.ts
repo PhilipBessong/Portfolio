fad44abaa2c57089b0a85d983c9bcc7d17d672e9
PhilipBessong <76492088+PhilipBessong@users.noreply.github.com>
2026-01-03 08:42:18 +0200

    hosting

--- ThePlum/Site/theplum/src/app/app.routes.server.ts
+++ ThePlum/Site/theplum/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'room/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      // Angular needs this list to know which HTML files to build at compile time
+      return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
